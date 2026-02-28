f30151663602757328e9b5752a13db76df1921fd
Quiescent <72737612+QuiescentTrip@users.noreply.github.com>
2026-02-28 15:24:20 +0100

    lol

--- src/lib/data/siteUpdates.ts
+++ src/lib/data/siteUpdates.ts
@@ -1,5 +1,6 @@
 export const updates = [
     { date: '02/23', text: 'added music player' },
     { date: '02/22', text: 'first commit of website' },
-    { date: '02/28', text: 'added games page' }
+    { date: '02/28', text: 'added games page' },
+    { date: '02/28', text: 'added guestbook' }
 ];
