daef97c3dee17a3fd185fb70bd095cc401a923a7
Quiescent <72737612+QuiescentTrip@users.noreply.github.com>
2026-02-28 12:37:51 +0100

    changed site updates

--- src/lib/data/siteUpdates.ts
+++ src/lib/data/siteUpdates.ts
@@ -1,4 +1,5 @@
 export const updates = [
     { date: '02/23', text: 'added music player' },
-    { date: '02/22', text: 'first commit of website' }
+    { date: '02/22', text: 'first commit of website' },
+    { date: '02/28', text: 'added games page' }
 ];
